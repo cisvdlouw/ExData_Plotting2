e367c8096997c65c3934e827beb7bc8d75f88b3f
cisvdlouw <cisvdlouw@msn.com>
2017-08-18 16:46:01 +0200

    -Added install packages

--- load_data.R
+++ load_data.R
@@ -1,4 +1,9 @@
 ##Small script to load the data so that it works with my scripts
 ##Assumption that data is in the working directory
+##Also installs required packages
+
+install.packages("dplyr")
+install.packages("ggplot2")
+
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
